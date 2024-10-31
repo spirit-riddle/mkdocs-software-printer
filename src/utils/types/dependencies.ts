4919af7dfba63ccdb8a1eff581e00bdc2376225b
Michael Hunt <1001flextape@gmail.com>
2024-10-31 10:09:31 -0700

    final touches on removing dependencies from research environment.

--- src/utils/types/dependencies.ts
+++ src/utils/types/dependencies.ts
@@ -3,12 +3,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'yaml';
-import GroqClient from 'groq-sdk'; // For blueprintAi
 
 export interface Dependencies {
   fs: typeof fs;
   path: typeof path;
   yaml: typeof yaml;
-  // blueprintAiClient: GroqClient;
   // Add other dependencies as needed
 }
