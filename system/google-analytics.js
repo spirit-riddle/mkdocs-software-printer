3db11ab3ebed1e24ea764180c54b099bfa6d54a8
spirit-riddle <spirit-riddle@users.noreply.github.com>
2024-11-14 16:58:43 +0000

    deploy: 05ba437fba32170b0adbde947f07bc997a142866

---
+++ system/google-analytics.js
@@ -0,0 +1,5 @@
+// google-analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-NKZN8BZHQV'); // Replace with your Google Analytics ID
